a9a4bcd3568b4d3e2521a0aa1bf0e3c46a61c731
Peter Scarth <peter.scarth@gmail.com>
2016-03-05 18:46:23 +1000

    Updated app to use lib subdirectory and requirements.txt

---
+++ app/appengine_config.py
@@ -0,0 +1,7 @@
+from google.appengine.ext import vendor
+
+# Add any libraries installed in the "lib" folder.
+# Make the lib directory and install them first with:
+# pip install -t lib -r requirements.txt
+vendor.add('lib')
+
